285ca2aefb03d8c76290e8dd4b73e5efec431028
jithjayan <jithjayan9341@gmail.com>
2024-10-22 23:10:03 +0530

    Update urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -23,5 +23,10 @@ urlpatterns = [
     path('fun1',views.fun1),
     path('fun2/<int:a>/<int:b>',views.fun2),
     path('tnum/<int:num>',views.tnum),
-    path('city/',views.city)
+    path('city/<a>',views.city),
+    path('day/<int:a>',views.day),
+    path('bonus/<int:a>/<int:b>',views.bonus),
+    path('tax/<int:a>',views.tax),
+    path('bill/<int:a>',views.bill),
+    
 ]
