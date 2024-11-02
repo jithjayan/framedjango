5255ac062966560e0bcd304edbc56013be46ba41
jithjayan <jithjayan9341@gmail.com>
2024-11-02 09:22:12 +0530

    session

--- eshop/app/views.py
+++ eshop/app/views.py
@@ -1,27 +1,48 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
+from .models import *
 # Create your views here.
 def shop_login(req):
+    if 'shop' in req.session:
+         return redirect(shop_home)
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['paswd']
         data=authenticate(username=uname,password=password)
         if data:
             login(req,data)
+            req.session['shop']=uname
             return redirect(shop_home)
-        
     else:
         return render(req,'login.html')
     
 def shop_logout(req):
+        req.session.flush()
         logout(req)
         return redirect(shop_login)
     
 def shop_home(req):
-    return render(req,'shop/home.html')
+    if 'shop' in req.session:
+        return render(req,'shop/home.html')
+    else:
+         return redirect(shop_login)
 
 def add_product(req):
-     if req.method=='POST':
-          pass
+     if 'shop' in req.session:
+          
+        if req.method=='POST':
+            id=req.POST['pro_id']
+            name=req.POST['name']
+            price=req.POST['price']
+            offer_price=req.POST['offer_price']
+            img=req.FILES['img']
+            disp=req.POST['disp']
+            data=product.objects.create(pro_id=id,name=name,price=price,offer_price=offer_price,img=img,disp=disp)
+            data.save()
+            return redirect(shop_home)
+            
+        else:
+            return render(req,'shop/add_product.html')
      else:
-          return render(req,'shop/add_product.html')
+         return redirect(shop_login)
+
